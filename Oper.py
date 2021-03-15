5936a7f4c22f3532e073be6f861ca7c301427b9d
AlSakharoB <71816331+AlSakharoB@users.noreply.github.com>
2021-03-15 13:12:39 +0300

    Update Oper.py

--- Oper.py
+++ Oper.py
@@ -24,21 +24,21 @@ if filename != 'operation':
 if filename + file_extension == "operation.it":
     a = ReadOpeartionFile.Read(0).split()
     b = ReadOpeartionFile.Read(1).split()
-if b[0] == "R":
-    if int(b[2]) > int(b[4]):
-        b[4], b[2] = b[2], b[4]
-    if int(b[1]) > int(b[3]):
-        b[3], b[1] = b[1], b[3]
-    DrawFillRec.DrawFillRec(a, b)
-if b[0] == 'C':
-    DrawFullCircle.DrawFullCircle(a, b)
-if b[0] == 'c':
-    DrawCircle.DrawCircle(a, b)
-if b[0] == "L":
-    DrawLine.DrawLine(a, b)
-if b[0] == 'r':
-    if int(b[2]) > int(b[4]):
-        b[4], b[2] = b[2], b[4]
-    if int(b[1]) > int(b[3]):
-        b[3], b[1] = b[1], b[3]
-    DrawRec.DrawRec(a, b)
+    if b[0] == "R":
+        if int(b[2]) > int(b[4]):
+            b[4], b[2] = b[2], b[4]
+        if int(b[1]) > int(b[3]):
+            b[3], b[1] = b[1], b[3]
+        DrawFillRec.DrawFillRec(a, b)
+    if b[0] == 'C':
+        DrawFullCircle.DrawFullCircle(a, b)
+    if b[0] == 'c':
+        DrawCircle.DrawCircle(a, b)
+    if b[0] == "L":
+        DrawLine.DrawLine(a, b)
+    if b[0] == 'r':
+        if int(b[2]) > int(b[4]):
+            b[4], b[2] = b[2], b[4]
+        if int(b[1]) > int(b[3]):
+            b[3], b[1] = b[1], b[3]
+        DrawRec.DrawRec(a, b)
